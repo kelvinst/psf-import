00d36ec80afe6bd2fb58d505fbfe85aa2e5c2641
Kelvin Raffael Stinghen <kelvin.stinghen@gmail.com>
2013-09-11 13:47:09 -0300

    Some wizard adjustments

--- src/org/psf/importer/wizard/ImportProjectSetWizard.java
+++ src/org/psf/importer/wizard/ImportProjectSetWizard.java
@@ -1,21 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.psf.importer.wizard;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
 
-public class ImportProjectSetWizard extends Wizard {
+public class ImportProjectSetWizard extends Wizard implements IImportWizard {
+	
+	ImportProjectSetSelectionPage selectionPage;
 
 	public ImportProjectSetWizard() {
-		setWindowTitle("New Wizard");
+		super();
 	}
 
-	@Override
-	public void addPages() {
-		addPage(new ImportProjectSetSelectionPage());
-	}
-
-	@Override
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
 	public boolean performFinish() {
-		return false;
+		IFile file = selectionPage.createNewFile();
+        if (file == null)
+            return false;
+        return true;
 	}
+	 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		setWindowTitle("File Import Wizard"); //NON-NLS-1
+		setNeedsProgressMonitor(true);
+		selectionPage = new ImportProjectSetSelectionPage("Import File",selection); //NON-NLS-1
+	}
+	
+	/* (non-Javadoc)
+     * @see org.eclipse.jface.wizard.IWizard#addPages()
+     */
+    public void addPages() {
+        super.addPages(); 
+        addPage(selectionPage);        
+    }
 
 }
