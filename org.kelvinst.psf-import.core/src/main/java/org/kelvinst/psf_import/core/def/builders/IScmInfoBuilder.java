50af71de5397cc7c0ec4210a44b2943bda9d6671
Kelvin Raffael Stinghen <kelvin.stinghen@gmail.com>
2013-09-26 14:51:22 -0300

    Moving base sources to psf-def

--- org.kelvinst.psf-import.core/src/main/java/org/kelvinst/psf_import/core/def/builders/IScmInfoBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.kelvinst.psf_import.core.def.builders;
-
-import org.kelvinst.psf_import.core.def.IScmInfo;
-
-public interface IScmInfoBuilder {
-
-	IScmInfo build(String reference);
-	
-}
